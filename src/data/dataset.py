c1710214a3dc95453854cd8f4bd9cccdf99f34e9
rishab-sharma <rishusharma.sharma7@gmail.com>
2022-07-04 05:14:10 +0530

    update

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -5,17 +5,33 @@ from glob import glob
 
 from torchvision.datasets.vision import VisionDataset
 from torchvision.io import read_image
+import torchvision.transforms as transforms
 
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
+train_transform = transforms.Compose([transforms.Resize(255),transforms.RandomResizedCrop(224),
+                                transforms.CenterCrop(224),
+                                transforms.RandomHorizontalFlip(),
+                                transforms.ColorJitter(),
+                                transforms.ToTensor()])
+test_transform = transforms.Compose([
+                                transforms.Resize(255),
+                                transforms.CenterCrop(224),
+                                transforms.ToTensor()])
 
 class CustomVideoDataset(VisionDataset):
-    def __init__(self, data_dir, ann_file, transform=None, target_transform=None, val=False):
+    def __init__(self, data_dir, ann_file, target_transform=None, val=False):
         
         self.data_dir = data_dir
         dir_len = len(glob(os.path.join(data_dir, "*")))
+
         if not val:
+            transform=train_transform
             self.frame_dirs = glob(os.path.join(data_dir, "*"))[:int(dir_len*0.8)]
         else:
+            transform=test_transform
             self.frame_dirs = glob(os.path.join(data_dir, "*"))[int(dir_len*0.8):]
    
         f = open(ann_file)
