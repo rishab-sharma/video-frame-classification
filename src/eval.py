8d6a34f6f007dfe8726b54372e5a617bc7b49c24
rishab-sharma <rishusharma.sharma7@gmail.com>
2022-07-04 06:19:23 +0530

    update

--- src/eval.py
+++ src/eval.py
@@ -15,13 +15,8 @@ def evaluate(model, criterion, data_loader, device, num_classes):
             output = model(image)
             loss = criterion(output, target)
             epoch_loss.append(loss.item())
-
-            if config.MODEL_NAME in ['u2net']:
-                output = F.log_softmax(output[0], dim=1)
-            elif config.MODEL_NAME in ['dpn98', 'se_resnext101_32x4d']:
-                output = output
-            else:
-                output = output['out']
+            
+            output = output
 
     
     avg_loss = sum(epoch_loss)/len(epoch_loss) if len(epoch_loss) else 0.0
